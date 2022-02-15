9e3497143429ad44a3f720a49d074d970c216a54
simjj1907 <jjsim1907@gmail.com>
2022-02-15 17:08:21 +0800

    hello

--- Pythonproj.py
+++ Pythonproj.py
@@ -1,3 +1,25 @@
 print ("hello")
 print("hello again")
 print('test')
+
+a = "Malaysia"
+b = "Singapore"
+c = "Vietnam"
+d = "Indonesia"
+
+while x <= 108 {
+    Singapore = sum
+    Vietname = sum
+
+p = max()
+}
+
+table = {
+  'Years': ["1978 - 1987" , "1988 - 1997" , "1998 - 2007" , "2008 - 2017"],
+  'Regions' : [a , b , c , d],
+  'Data' : '''import Int monthly vistor.xlsx'''
+}
+
+myvar = pandas.DataFrame(table)
+
+print(myvar)
